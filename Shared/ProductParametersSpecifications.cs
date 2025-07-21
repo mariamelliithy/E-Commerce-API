de6dd4ef9ff3caee717eec2ac97452770d609217
mariamelliithy <noraali182003@gmail.com>
2025-07-21 18:52:19 +0300

    no message

--- Shared/ProductParametersSpecifications.cs
+++ Shared/ProductParametersSpecifications.cs
@@ -13,6 +13,12 @@ namespace Shared
         public int? TypeId { get; set; }
         public int? BrandId { get; set; }
         public ProductSortOptions? Sort { get; set; }
+
+
+
+
+
+
     }
     public enum ProductSortOptions
     {
